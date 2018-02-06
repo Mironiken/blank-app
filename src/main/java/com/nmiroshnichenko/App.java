fc6c19496940aee4385d0340a2c0f752fa776397
Mironiken <mironike@yandex.ru>
2018-02-06 14:32:11 +0300

    Add blank mvn project

---
+++ src/main/java/com/nmiroshnichenko/App.java
@@ -0,0 +1,13 @@
+package com.nmiroshnichenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
